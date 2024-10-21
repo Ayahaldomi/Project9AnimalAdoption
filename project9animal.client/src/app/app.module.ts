93225f536297c268a04b7eb6fccb0c72ce982969
LeenAbudaboos <leenabodbos@gmail.com>
2024-10-21 12:47:02 +0300

    home and footer and nav

--- project9animal.client/src/app/app.module.ts
+++ project9animal.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home/home.component';
+import { NavBarComponent } from './home/nav-bar/nav-bar.component';
+import { FooterComponent } from './home/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
