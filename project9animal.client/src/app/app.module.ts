c44d76124dce80ac0ab88f1317d4a497713557d4
LeenAbudaboos <leenabodbos@gmail.com>
2024-10-21 15:06:01 +0300

    about peag

--- project9animal.client/src/app/app.module.ts
+++ project9animal.client/src/app/app.module.ts
@@ -7,19 +7,30 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home/home.component';
 import { NavBarComponent } from './home/nav-bar/nav-bar.component';
 import { FooterComponent } from './home/footer/footer.component';
+import { AbuotComponent } from './home/abuot/abuot.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     NavBarComponent,
-    FooterComponent
+    FooterComponent,
+    AbuotComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: "", component: HomeComponent, pathMatch: "full" },
+      { path: "abuot", component: AbuotComponent },
+
+
+    ])
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+   
+ 
 })
 export class AppModule { }
