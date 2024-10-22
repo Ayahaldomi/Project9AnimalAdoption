20baf61d8df17605deadba33806fa26c34244daa
LeenAbudaboos <leenabodbos@gmail.com>
2024-10-22 09:48:16 +0300

    register

---
+++ Project9Animal.Server/DTOs/loginDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Project9Animal.Server.DTOs
+{
+    public class loginDTO
+    {
+
+        public string? Password { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
