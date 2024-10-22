9a54d66c4fa66b608dc2b320beb85eb73c6e3957
BassamBanyAli <bassambanyali@gmail.com>
2024-10-22 15:14:16 +0300

    bassam

---
+++ Project9Animal.Server/Controllers/BassamController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Project9Animal.Server.Models;
+
+namespace Project9Animal.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BassamController : ControllerBase
+    {
+        private readonly MyDbContext _db;
+         public BassamController(MyDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+
+            var blog = _db.SuccessStories.ToList();
+            return Ok(blog);
+        }
+    }
+}
--- project9animal.client/src/app/Bassam/blog/blog.component.ts
+++ project9animal.client/src/app/Bassam/blog/blog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { UrlBassamService } from '../BassamUrl/url-bassam.service';
 
 @Component({
   selector: 'app-blog',
@@ -6,5 +7,22 @@ import { Component } from '@angular/core';
   styleUrl: './blog.component.css'
 })
 export class BlogComponent {
+
+  successStories: any[] = [];
+  constructor(private urlBassamService: UrlBassamService) { }
+
+
+  ngOnInit(): void {
+    this.urlBassamService.getSuccessStories().subscribe(
+      data => {
+        this.successStories = data;
+        console.log(this.successStories);
+      },
+      error => {
+        console.error('Error fetching success stories:', error);
+      }
+    );
+  }
+
 
 }
