dbf327f4b70384ad1de6f1e98923b789ad7a6b57
Rawaahababneh63 <rawaahabedelrahman@gmail.com>
2024-10-22 15:51:42 +0300

    addanmimals

--- project9animal.client/src/app/Admin/add-animals/add-animals.component.ts
+++ project9animal.client/src/app/Admin/add-animals/add-animals.component.ts
@@ -1,39 +1,95 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RawaahServicesService } from '../../URL-serices/rawaah-services.service';
 
 @Component({
   selector: 'app-add-animals',
   templateUrl: './add-animals.component.html',
-  styleUrl: './add-animals.component.css'
+  styleUrls: ['./add-animals.component.css']
 })
-export class AddAnimalsComponent {
-  ngOnInit() {
+export class AddAnimalsComponent implements OnInit {
+  categories: any[] = [];
+  shelters: any[] = [];    
+  image: any;
 
-  }
+  selectedCategoryId: string | undefined; 
+  selectedShelterId: string | undefined;  
+
+  isLoading: boolean = false;  
 
   constructor(private _ser: RawaahServicesService) { }
 
-
-  image: any
-  changeImage(event: any) {
-    debugger
-    this.image = event.target.files[0]
-
+  ngOnInit() {
+    this.loadCategories();
+    this.loadShelters();
   }
 
+  loadCategories() {
+    this._ser.getCategories().subscribe(
+      (categories) => {
+        this.categories = categories;
+      },
+      (error) => {
+        alert('Error loading categories: ' + error.message);
+      }
+    );
+  }
+
+  loadShelters() {
+    this._ser.getShelters().subscribe(
+      (shelters) => {
+        this.shelters = shelters;
+      },
+      (error) => {
+        alert('Error loading shelters: ' + error.message);
+      }
+    );
+  }
+
+  changeImageEvent(event: any) {
+    this.image = event.target.files[0];
+  }
 
   addAnimalsAdmin(data: any) {
-
-    var form = new FormData();
-
-    for (let key in data) {
-      form.append(key, data[key])
+    // قم بإزالة التحقق من الحقول هنا
+    const form = new FormData();
+    
+    if (this.selectedCategoryId) {
+      form.append('CategoryId', this.selectedCategoryId);
+    }
+    
+    if (this.selectedShelterId) {
+      form.append('ShelterId', this.selectedShelterId);
     }
 
-    form.append("Animals Image", this.image)
+    for (let key in data) {
+      if (data[key]) { // تحقق من أن القيمة ليست فارغة
+        form.append(key, data[key]);
+      }
+    }
 
-    this._ser.addAnimalsAdmin(form).subscribe(() => {
-      alert("Animals added successfully")
-    })
+    if (this.image) {
+      form.append('AnimalsImage', this.image);
+    }
+
+    this.isLoading = true;  // Start loading
+
+    this._ser.addAnimalsAdmin(form).subscribe(
+      () => {
+        alert('Animal added successfully');
+        this.resetForm();  
+      },
+      (error) => {
+        alert('Error adding animal: ' + error.message);
+      },
+      () => {
+        this.isLoading = false; 
+      }
+    );
+  }
+
+  resetForm() {
+    this.selectedCategoryId = undefined;
+    this.selectedShelterId = undefined;
+    this.image = null;
   }
 }
--- project9animal.client/src/app/Admin/edit-animals/edit-animals.component.ts
+++ project9animal.client/src/app/Admin/edit-animals/edit-animals.component.ts
@@ -1,3 +1,89 @@
+//import { Component, OnInit } from '@angular/core';
+//import { ActivatedRoute } from '@angular/router';
+//import { RawaahServicesService } from '../../URL-serices/rawaah-services.service';
+
+//@Component({
+//  selector: 'app-edit-animals',
+//  templateUrl: './edit-animals.component.html',
+//  styleUrls: ['./edit-animals.component.css']
+//})
+//export class EditAnimalsComponent implements OnInit {
+//  param: any;
+//  imageFile: any;
+//  animalData: any = {};
+//  categories: any[] = [];
+//  shelters: any[] = [];
+
+//  constructor(
+//    private _ser: RawaahServicesService,
+//    private _active: ActivatedRoute
+//  ) {
+//    this.param = this._active.snapshot.paramMap.get('id');
+//  }
+
+//  ngOnInit() {
+//    if (this.param) {
+//      this.loadAnimalData();
+//      this.loadCategoriesAndShelters();
+//    } else {
+//      alert('Animal ID not found in the route parameters.');
+//      console.error('Animal ID not found.');
+//    }
+//  }
+
+//  loadAnimalData() {
+//    this._ser.getAnimalById(this.param!).subscribe(
+//      (data) => {
+//        this.animalData = data;
+//        // Load the selected category and shelter based on the fetched animal data
+//        this.animalData.CategoryId = data.CategoryId;
+//        this.animalData.ShelterId = data.ShelterId;
+//      },
+//      (error) => {
+//        alert('Error loading animal data: ' + error.message);
+//      }
+//    );
+//  }
+
+//  changeImageEvent(event: any) {
+//    this.imageFile = event.target.files[0];
+//  }
+
+//  loadCategoriesAndShelters() {
+//    this._ser.getCategories().subscribe(
+//      (categories) => (this.categories = categories),
+//      (error) => alert('Error loading categories: ' + error.message)
+//    );
+
+//    this._ser.getShelters().subscribe(
+//      (shelters) => (this.shelters = shelters),
+//      (error) => alert('Error loading shelters: ' + error.message)
+//    );
+//  }
+
+//  UpdateAnimalsAdmin(data: any) {
+//    const form = new FormData();
+//    for (let key in data) {
+//      if (data.hasOwnProperty(key)) {
+//        form.append(key, data[key]);
+//      }
+//    }
+//    if (this.imageFile) {
+//      form.append('AnimalsImage', this.imageFile);
+//    }
+
+//    this._ser.UpdateAnimalsAdmin(this.param!, form).subscribe(
+//      (response) => {
+//        alert('Animal updated successfully!');
+//      },
+//      (error) => {
+//        alert('Error updating animal: ' + error.message);
+//      }
+//    );
+//  }
+//}
+
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { RawaahServicesService } from '../../URL-serices/rawaah-services.service';
@@ -10,53 +96,81 @@ import { RawaahServicesService } from '../../URL-serices/rawaah-services.service
 export class EditAnimalsComponent implements OnInit {
   param: any;
   imageFile: any;
-  animalData: any; 
+  animalData: any = {};
+  categories: any[] = []; 
+  shelters: any[] = [];   
 
-  constructor(private _ser: RawaahServicesService, private _active: ActivatedRoute) {
-
+  constructor(
+    private _ser: RawaahServicesService,
+    private _active: ActivatedRoute
+  ) {
     this.param = this._active.snapshot.paramMap.get('id');
   }
 
   ngOnInit() {
- 
-    this.loadAnimalData();
+    if (this.param) {
+      this.loadAnimalData();
+      this.loadCategoriesAndShelters();
+    } else {
+      alert('Animal ID not found in the route parameters.');
+      console.error('Animal ID not found.');
+    }
   }
 
   loadAnimalData() {
-    this._ser.getAnimalById(this.param).subscribe((data) => {
-      this.animalData = data;
-      this.imageFile = data.ServiceImage;
-    });
+    this._ser.getAnimalById(this.param!).subscribe(
+      (data) => {
+        this.animalData = data;
+        // Load the selected category and shelter based on the fetched animal data
+        this.animalData.CategoryId = data.CategoryId;
+        this.animalData.ShelterId = data.ShelterId;
+      },
+      (error) => {
+        alert('Error loading animal data: ' + error.message);
+      }
+    );
   }
 
   changeImageEvent(event: any) {
     this.imageFile = event.target.files[0];
   }
 
+  loadCategoriesAndShelters() {
+    this._ser.getCategories().subscribe(
+      (categories) => (this.categories = categories),
+      (error) => alert('Error loading categories: ' + error.message)
+    );
+
+    this._ser.getShelters().subscribe(
+      (shelters) => (this.shelters = shelters),
+      (error) => alert('Error loading shelters: ' + error.message)
+    );
+  }
+
   UpdateAnimalsAdmin(data: any) {
     const form = new FormData();
-
-
     for (let key in data) {
       if (data.hasOwnProperty(key)) {
         form.append(key, data[key]);
       }
     }
-
-
     if (this.imageFile) {
-      form.append("ServiceImage", this.imageFile);
+      form.append('AnimalsImage', this.imageFile);
     }
 
-
-    this._ser.UpdateAnimalsAdmin(this.param, form).subscribe({
-      next: (response) => {
-        alert("Animal Updated Successfully");
+    this._ser.UpdateAnimalsAdmin(this.param!, form).subscribe(
+      (response) => {
+        alert('Animal updated successfully!');
       },
-      error: (error) => {
-        console.error("Error updating animal:", error);
-        alert("An error occurred while updating the animal.");
+      (error) => {
+        alert('Error updating animal: ' + error.message);
       }
-    });
+    );
   }
 }
+
+
+
+
+
+
